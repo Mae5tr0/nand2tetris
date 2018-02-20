f683912d55e4b4fe00c4dcd3143900299b05dae0
Mae5tr0 <i.kirill.maestro@gmail.com>
2018-02-20 15:52:19 +0700

    Complete project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,91 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+// while (true) {
+//   keyboard_char = read_keyboard()
+//   if (keyboard_char != 0) {
+//      fill_screen()
+//      next
+//   }
+//  clear_screen()
+// }
+
+    // screen size
+    @8192    
+    D=A
+    @n
+    M=D
+
+(LOOP)                  // while (true)
+    @KBD           // keyboard_char = read_keyboard()
+    D=M
+
+    @CLEAN_SCREEN         // if (keyboard_char != 0) {
+    D;JEQ
+
+    // fill_screen
+    // for (i=0; i<8192; i++) {
+    //   M[SCREEN + i] = -1   
+    // }
+    @i
+    M=0
+
+ (FILL_SCREEN_LOOP) 
+    @i
+    D=M
+    @n
+    D=D-M
+    @END_FILL_SCREEN_LOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i    
+    A=D+M
+    M=-1
+
+    @i
+    M=M+1
+
+    @FILL_SCREEN_LOOP
+    0;JMP
+ (END_FILL_SCREEN_LOOP)
+
+
+    @LOOP 
+    0;JMP                     
+
+(CLEAN_SCREEN)
+    // clear_screen
+    // for (i=0; i<8192; i++) {
+    //   M = 0   
+    // }
+
+    @i
+    M=0
+
+ (CLEAR_SCREEN_LOOP) 
+    @i
+    D=M
+    @n
+    D=D-M
+    @END_CLEAR_SCREEN_LOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i    
+    A=D+M
+    M=0
+
+    @i
+    M=M+1
+
+    @CLEAR_SCREEN_LOOP
+    0;JMP    
+ (END_CLEAR_SCREEN_LOOP)
+
+    @LOOP
+    0;JMP 
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,65 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//
+// R2 = 0
+// negative = false
+// if (R1 < 0) {
+//  R1 = -R1
+//  negative = true
+// }
+// while (R1 > 0) {
+//  R2 = R2 + R0
+//  R1--    
+// }
+// if (negative) {
+//  R2 = -R2   
+// }
+
+    @R2                 // R2 = 0
+    M=0 
+
+    @negative           // negative = 0
+    M=0
+
+    @R1                 // if (R1 < 0) {
+    D=M        
+    @LOOP
+    D;JGT
+
+    @negative           // nagative = true
+    M=1
+
+    @R1                 // R1 = -R1
+    M=-M
+
+(LOOP)                  // while (R1 != 0)
+    @R1
+    D=M        
+
+    @END_LOOP
+    D;JLE        
+
+    @R0                 // R2 = R2 + R0
+    D=M
+    @R2
+    M=D+M
+
+    @R1                 // R1--
+    M=M-1                            
+
+    @LOOP
+    0;JMP
+(END_LOOP)    
+
+    @negative           // if (negative)
+    D=M
+    @END
+    D;JEQ
+
+    @R2                 // R2 = -R2
+    M=-M
+
+(END)
+    @END
+    0;JMP
